f202362921167423364f18e976c790da99295e1e
Gustavo Maia <ms.gustavo@outlook.com>
2023-08-29 17:02:45 -0300

    router and views

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./views/Home";
+import Settings from "./views/Settings";
+import Login from "./views/Login";
+import Register from "./views/Register";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Navbar />
+      <div className="content-wrapper">
+        <Routes>
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,24 +1,41 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <div className="chat-navbar">
       <nav className="chat-navbar-inner">
         <div className="chat-navbar-inner-left">
-          <a href="/" className="btn btn-outline-success ml-2">
+          <button
+            onClick={() => navigate(-1)}
+            className="btn btn-outline-warning"
+          >
+            Back
+          </button>
+          <button
+            onClick={() => navigate("/")}
+            className="btn btn-outline-primary ml-2"
+          >
+            Dashboard
+          </button>
+          <button
+            onClick={() => navigate("/settings")}
+            className="btn btn-outline-success ml-2"
+          >
             Settings
-          </a>
+          </button>
         </div>
         <div className="chat-navbar-inner-right">
           <span className="logged-in-user">Hi User</span>
           <button
-            onClick={() => {}}
-            className="btn btn-sm btn-outline-danger ml-2"
+            onClick={() => navigate("/register")}
+            className="btn btn-outline-danger ml-2"
           >
             Logout
           </button>
           <button
-            onClick={() => {}}
-            className="btn btn-sm btn-outline-success ml-2"
+            onClick={() => navigate("/login")}
+            className="btn btn-outline-success ml-2"
           >
             Login
           </button>
