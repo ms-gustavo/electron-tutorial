13371773acf0254716416751e6f4633adfa49ac4
Gustavo Maia <ms.gustavo@outlook.com>
2023-09-05 12:28:52 -0300

    dotenv

--- src/js/db/firestore.js
+++ src/js/db/firestore.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCkAklHyGv60BYHt6sDMzEpuXcrBQ-Pqu8",
-  authDomain: "electron-chat-9fb07.firebaseapp.com",
-  projectId: "electron-chat-9fb07",
-  storageBucket: "electron-chat-9fb07.appspot.com",
-  messagingSenderId: "1065041782458",
-  appId: "1:1065041782458:web:72651fd78cf79f6dcc0483",
-  measurementId: "G-M12C7Z2SYB",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 export const { Timestamp } = firebase.firestore;
