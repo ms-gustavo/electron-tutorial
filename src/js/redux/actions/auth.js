48d3cb23795a78362eb62d5f9e820d0468ee45ce
Gustavo Maia <ms.gustavo@outlook.com>
2023-09-07 16:52:05 -0300

    displaying errors, fix problem

--- src/js/redux/actions/auth.js
+++ src/js/redux/actions/auth.js
@@ -13,7 +13,8 @@ export const loginUser = (formData) => (dispatch) => {
   dispatch({ type: "AUTH_LOGIN_INIT" });
   return api
     .login(formData)
-    .then((_) => dispatch({ type: "AUTH_LOGIN_SUCCESS" }));
+    .then((_) => dispatch({ type: "AUTH_LOGIN_SUCCESS" }))
+    .catch((error) => dispatch({ type: "AUTH_LOGIN_ERROR", error }));
 };
 
 export const logoutUser = () => (dispatch) =>
