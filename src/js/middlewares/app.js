4584c2a6e84645f15bea34d60054b2840a354103
Gustavo Maia <ms.gustavo@outlook.com>
2023-09-15 10:56:34 -0300

    Display notifications

--- src/js/middlewares/app.js
+++ src/js/middlewares/app.js
@@ -1,10 +1,15 @@
+import notifications from "../utils/notifications";
+
 export default (store) => (next) => (action) => {
   const state = store.getState();
 
   switch (action.type) {
     case "APP_IS_ONLINE":
     case "APP_IS_OFFLINE": {
-      alert("Displaying notification");
+      Notification.show({
+        title: `Connection status:`,
+        body: action.isOnline ? `Online` : `Offline`,
+      });
     }
   }
   next(action);
