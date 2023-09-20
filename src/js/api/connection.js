b412b285738f2e8c52bd095be582fce3184a76ce
Gustavo Maia <ms.gustavo@outlook.com>
2023-09-20 11:40:19 -0300

    Save user online status to firestore

--- src/js/api/connection.js
+++ src/js/api/connection.js
@@ -1,5 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/database";
+import db from "../db/firestore";
+
+export const getOnlineStatus = (isOnline) => {
+  return {
+    state: isOnline ? "online" : "offline",
+    lastChanged: firebase.firestore.FieldValue.serverTimestamp(),
+  };
+};
+
+export const setUserOnlineStatus = (uid, isOnline) => {
+  const userRef = db.doc(`/profiles/${uid}`);
+  const updatedData = getOnlineStatus(isOnline);
+  return userRef.update(updatedData);
+};
 
 export const onConnectionChanged = (onConnection) =>
   firebase
