c1eab99e761fdf05a2693d55ed2706a2d7225e5b
Gustavo Maia <ms.gustavo@outlook.com>
2023-09-22 11:53:44 -0300

    cleanup after logout

--- src/js/redux/store/index.js
+++ src/js/redux/store/index.js
@@ -1,19 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
 import chatReducer from "../reducers/chats";
 import authReducer from "../reducers/auth";
 import appReducer from "../reducers/app";
 import appMiddleware from "../../middlewares/app";
+import { types } from "../types";
 
 export default function initStore() {
   const middlewares = [thunkMiddleware, appMiddleware];
 
+  const mainReducer = combineReducers({
+    chats: chatReducer,
+    auth: authReducer,
+    app: appReducer,
+  });
+
+  const rootReducer = (state, action) => {
+    if (action.type === types.AUTH_LOGOUT_SUCCESS) {
+      state = undefined;
+    }
+
+    return mainReducer(state, action);
+  };
+
   const store = configureStore({
-    reducer: {
-      chats: chatReducer,
-      auth: authReducer,
-      app: appReducer,
-    },
+    reducer: rootReducer,
     middleware: [...middlewares],
   });
 
